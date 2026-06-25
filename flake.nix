b9aef87baa623ce86983fa5a3e36ea9a4f3bda05
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-06-25 08:57:50 -0400

    bump nixpkgs-master: d233902 → 567a49d

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     # buildZxScriptFromFile, gomod2nix's buildGoApplication, etc. are
     # present without a manual overlays list (see amarbel-llc/eng#60).
     igloo.url = "github:amarbel-llc/igloo";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
 
     # `nix fmt` entry point. Config lives in ./treefmt.nix.
